bfb8db938ba93fb887ce040366571f3715a5dfd4
Mohammed Khaled <kingm7md112@gmail.com>
2023-03-16 07:24:05 +0200

    Visual improvements to the bio section, bugfixes elsewhere

--- js/app.js
+++
@@ -1,5 +0,0 @@
-// Function used for the "Bio" button:
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
